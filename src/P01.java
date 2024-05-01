81826fbe5c0ce69637095493e77ecc6e16ea2b22
Meltem Avluk <ml@gmail.com>
2024-05-01 14:44:13 +0300

    first commit

---
+++ src/P01.java
@@ -0,0 +1,12 @@
+public class P01 {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba GitHub");
+
+
+        int a=120;
+        int b=180;
+        int c=a+b;
+        System.out.println("c = "+ c);
+    }
+}
